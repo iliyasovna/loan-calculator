5e17883744327aa457c738fc8b412a7e53ce8127
Nasyikat Mambetova <nasyikat.mambetova.kk@gmail.com>
2022-07-18 04:01:25 +0600

    added js

---
+++ loan_calc/js/app.js
@@ -0,0 +1,13 @@
+const form = document.querySelector('#loan-form');
+form.addEventListener('submit', calculateResults);
+
+function calculateResults(e) {
+    constamount = document.querySelector('#amount');
+    const interest = document.querySelector('#interest');
+    const years = document.querySelector('#years');
+    const monthlyPayment = document.querySelector("#monthly-payment");
+    
+
+
+    e.preventDefault();
+}
